56ea429b459c15a430eeaf53e9d7ce7a35cb0a7f
yago8397 <yago_rj@hotmail.com>
2018-05-03 19:49:56 -0300

    git inicial

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+<?php
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
 	<head>
@@ -8,7 +10,60 @@
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
 		<title>Loja Gaia</title>
 	</head>
-	<body>
-
+		<body>
+			<div class="container">
+			<header>
+			<div class="row">
+				<img style="height: 10%;width: 10%;" class="logo col-md-2" src="imagens/logo.jpg" >
+					<form class="col col-md-8">
+						<input style="width: 90%;" type="search" name="busca" placeholder="Procurar">
+						
+					</form>
+					<a href="cadastro.php">Cadastre-se</a>
+					<a href="login.php">Login</a>
+			</div>
+				
+				<nav>
+					<ul class="row">
+						<a class="col-md-2" href="index.php">Inicio</a></li>
+						<a class="col-md-2" href="tel.php">Telefonia</a></li>
+						<a class="col-md-2" href="informatica.php">Informatica</a></li>
+						<a class="col-md-2" href="eletro.php">Eletrodomesticos</a></li>
+						<a class="col-md-2" href="mov.php">Moveis</a></li>
+						<a class="col-md-2" href="entre.php">Entretenimento</a></li>
+					</ul>
+				</nav>			
+			</header>
+			<section>
+				<img style="height: 20%;width: 100%;" class="banner" src="imagens/banner.jpg">
+			<div class="destaques row col">				
+				<div class="card" style="width: 18rem;">				
+				  <img class="card-img-top" src="imagens/co.jpeg" alt="Card image cap">
+				  <div class="card-body">
+				    <p class="card-text">preço</p>
+				</div>
+				</div>			
+				<div class="card" style="width: 18rem;">
+				  <img class="card-img-top" src="imagens/cr.jpg" alt="Card image cap">
+				  <div class="card-body">
+				    <p class="card-text">preçpo</p>
+				</div>
+				</div>			
+				<div class="card" style="width: 18rem;">
+				  <img class="card-img-top" src="imagens/jg.jpg" alt="Card image cap">
+				  <div class="card-body">
+				    <p class="card-text">preço</p>
+				</div>
+				</div>				
+			</div>	
+			
+			</section>
+				
+			
+		
+			<footer>
+			
+			</footer>
+			</div>
 	</body>
 </html>
